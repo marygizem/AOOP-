d8dc253d74b3d32221ba628ed160282d9f09a91c
Mary grace Ramos <119553061+marygizem@users.noreply.github.com>
2026-06-08 08:48:54 +0800

    Delete main directory

--- main/java/dao/LeaveDAO.java
+++
@@ -1,6 +0,0 @@
-package dao;
-
-public interface LeaveDAO {
-    boolean appendLeaveRequest(String leaveCsvFile, String employeeNumber, String leaveType,
-                               String startDate, String endDate, String reason) throws Exception;
-}
